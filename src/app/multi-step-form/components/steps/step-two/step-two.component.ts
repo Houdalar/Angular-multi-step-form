1552622bd789b76a66d73a1b676b60b2cd29c2d8
Houdalar <houda.lariani@esprit.tn>
2024-07-07 23:04:12 +0200

    step 2

--- src/app/multi-step-form/components/steps/step-two/step-two.component.ts
+++ src/app/multi-step-form/components/steps/step-two/step-two.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
+import { ControlContainer, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @Component({
   selector: 'app-step-two',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule,MatSlideToggleModule,FormsModule ],
   templateUrl: './step-two.component.html',
-  styleUrl: './step-two.component.scss'
+  styleUrl: './step-two.component.scss',
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useFactory: () => inject(ControlContainer, { skipSelf: true })
+    }
+  ],
 })
 export class StepTwoComponent {
+  @Input({ required: true }) controlKey = '';
+  parentContainer = inject(ControlContainer);
+  plans = [
+    { id: 'arcade', name: 'Arcade', monthly: '$10/mo', yearly: '$90/yr', perks: '2 months free', icon: '/assets/images/icon-arcade.svg' },
+    { id: 'advanced', name: 'Advanced', monthly: '$15/mo', yearly: '$120/yr', perks: '2 months free', icon: '/assets/images/icon-advanced.svg'  },
+    { id: 'pro', name: 'Pro', monthly: '$20/mo', yearly: '$150/yr', perks: '2 months free', icon: '/assets/images/icon-pro.svg' }
+  ];
+  billingType = 'Monthly';
+  isYearly: boolean = false;
+  get parentFormGroup() {
+    return this.parentContainer.control as FormGroup;
+  }
 
+  ngOnInit() {
+    this.parentFormGroup.addControl(this.controlKey, new FormGroup({
+        selectedPlan: new FormControl(''), 
+        billingCycleControl: new FormControl(this.billingType === 'Yearly')
+    }));
+    
+    this.parentContainer.control?.get(this.controlKey)?.get('billingCycleControl')?.valueChanges.subscribe(value => {
+      this.isYearly = value;
+    });
+}
+
+
+  ngOnDestroy() {
+    this.parentFormGroup.removeControl(this.controlKey);
+  }
+
+  get currentPlan() {
+    return this.plans.find(plan => plan.id === this.parentFormGroup.get('selectedPlan')?.value);
+  }
+
+  updateBillingType() {
+    const isYearly = this.parentContainer.control!.get(this.controlKey)?.get('billingCycleControl')?.value;
+    this.billingType = isYearly ? 'Yearly' : 'Monthly';
+    this.parentContainer.control!.get(this.controlKey)?.get('selectedPlan')?.setValue(this.billingType);
+  }
+
+  onBillingTypeChange() {
+    const toggleControl = this.parentContainer.control!.get(this.controlKey)?.get('billingCycleControl');
+    toggleControl?.setValue(!toggleControl.value);
+    this.updateBillingType();
+  }
 }
--- src/app/multi-step-form/multi-step-form.component.ts
+++ src/app/multi-step-form/multi-step-form.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { StepIndicatorComponent } from './components/step-indicator/step-indicator.component';
 import { StepOneComponent } from './components/steps/step-one/step-one.component';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { StepTwoComponent } from './components/steps/step-two/step-two.component';
 
 @Component({
   selector: 'app-multi-step-form',
   standalone: true,
-  imports: [CommonModule, StepOneComponent, ReactiveFormsModule],
+  imports: [CommonModule, StepOneComponent, ReactiveFormsModule, StepTwoComponent],
   templateUrl: './multi-step-form.component.html',
   styleUrl: './multi-step-form.component.scss'
 })
